4bc75f7644918f8d16ef630eb712cbd966a4084d
Anton Latukha <anton.latukha@gmail.com>
2018-07-30 15:44:51 +0300

    upd conf

--- configuration.nix
+++ configuration.nix
@@ -153,6 +153,7 @@
       enable = true;
       liveRestore = true;
       extraOptions = "--experimental=true";
+      listenOptions = [ "/var/run/docker.sock" "tcp://127.0.0.1:2376/" ];
     };
 
     libvirtd = {
