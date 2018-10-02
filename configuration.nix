88cabab043967b3b033d06ad60631d9d6ae13a9b
Anton Latukha <anton.latukha@gmail.com>
2018-10-02 21:55:33 +0300

    upd conf

--- configuration.nix
+++ configuration.nix
@@ -214,12 +214,12 @@
   users.motd = ''
 #!/run/current-system/sw/bin/sh
  
-upSeconds="$(/run/current-system/sw/bin/cut -d. -f1 /proc/uptime)"
-secs=$((${upSeconds}%60))
-mins=$((${upSeconds}/60%60))
-hours=$((${upSeconds}/3600%24))
-days=$((${upSeconds}/86400))
-UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
+#upSeconds="$(/run/current-system/sw/bin/cut -d. -f1 /proc/uptime)"
+#secs=$((${upSeconds}%60))
+#mins=$((${upSeconds}/60%60))
+#hours=$((${upSeconds}/3600%24))
+#days=$((${upSeconds}/86400))
+#UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
  
 # get the load averages
 read one five fifteen rest < /proc/loadavg
