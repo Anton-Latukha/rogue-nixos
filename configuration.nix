d5651967f3dae2d00a8b9698454544badc96abea
Anton Latukha <anton.latukha@gmail.com>
2018-10-02 21:57:56 +0300

    upd conf

--- configuration.nix
+++ configuration.nix
@@ -219,7 +219,7 @@ secs=$(("$upSeconds"%60))
 mins=$(("$upSeconds"/60%60))
 hours=$(("$upSeconds"/3600%24))
 days=$(("$upSeconds"/86400))
-UPTIME=`printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs"`
+UPTIME="$(printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs")"
  
 # get the load averages
 read one five fifteen rest < /proc/loadavg
