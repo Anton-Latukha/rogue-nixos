bcc12a5200af770518d5b541542ab54f7de93e56
Anton Latukha <anton.latukha@gmail.com>
2018-11-18 19:53:47 +0200

    upd conf

--- derivations.nix
+++ derivations.nix
@@ -120,7 +120,6 @@ in {
 
     dos2unix    # Convert Windows special text symbols to Unix
 
-    gdrivefs    # Google Drive FUSE mounter
     ## Configuration management
     ansible
     terraform-full
