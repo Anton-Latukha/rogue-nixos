c9781804eb1124b6acb25c2f1f1f1d0ec5f09fcc
Anton Latukha <anton.latukha+gitlab@gmail.com>
2019-06-21 21:46:00 +0300

    upd conf

--- derivations.nix
+++ derivations.nix
@@ -236,7 +236,7 @@ in {
     kdeFrameworks.syntax-highlighting
     kwayland-integration
     filelight
-    ark    p7zip    	unar
+    ark    p7zip   rar 	unar
     kdeApplications.spectacle
     kdeApplications.kcalutils
     kdeFrameworks.kcmutils
