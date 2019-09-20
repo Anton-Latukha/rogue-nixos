feb0a5d8c6f56cc63ce55de7770ab53d02eb4c9f
Anton Latukha <anton.latukha@gmail.com>
2019-09-21 01:36:20 +0300

    upd conf

--- configuration.nix
+++ configuration.nix
@@ -75,7 +75,7 @@
   programs.fish.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  services.emacs.install = true;
+  services.emacs.emable = true;
   services.haveged.enable = true;
   services.openssh.enable = true;
   services.openssh.allowSFTP = false;
