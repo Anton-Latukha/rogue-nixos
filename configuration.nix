d3d8f5ecbb0527d63f2a4f55c73b307d4fdec76d
Anton Latukha <anton.latukha@gmail.com>
2019-09-23 01:16:11 +0300

    upd conf

--- configuration.nix
+++ configuration.nix
@@ -273,7 +273,7 @@
   services.jupyter.group = "pyro";
   services.jupyter.notebookDir = "~/src/notebooks/";
   services.jupyter.user = "pyro";
-  services.jypyter.user = [
+  services.jupyter.user = [
     "open('/etc/nixos/private/secrets/jupyter.pass', 'r', encoding='utf8').read().strip()"
   ];
 }
