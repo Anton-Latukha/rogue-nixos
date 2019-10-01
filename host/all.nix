9810645362562d1b1e84928a3ead806ca5b2a6cc
Anton Latukha <anton.latukha@gmail.com>
2019-10-01 22:03:31 +0300

    upd conf

--- host/all.nix
+++ host/all.nix
@@ -1,9 +1,41 @@
 { ... }:{
 
+  nixpkgs.config.allowUnfree = true;
+
   hardware.cpu.intel.updateMicrocode = true;
   hardware.enableAllFirmware = true;
 
   boot.cleanTmpDir = true;  # Clean /tmp on boot
   boot.tmpOnTmpfs = true;  # /tmp on ram drive
+
+  nix.autoOptimiseStore = true;    # Autodeduplicate files in store
+
+
+  services.fwupd.enable = true;
+
+  programs.iotop.enable = true;
+  programs.fish.enable = true;
+
+  services.geoclue2.enable = true;
+  services.localtime.enable = true;
+
+  location.provider = "geoclue2";
+
+  users.motd = ''
+
+
+                                                               ==    ##   ##    
+  {}            {}        __                                    ===   ## ##     
+ {}              {}       \ \ ___                            ========== ##   /  
+ {}              {}        \ \\  \  _____         ->             ##      ## //  
+ {}              {}         \ \\  \ \____\      ->->->         ##          //   
+{}                {}        / //   \  ____     ->->->->   ######          //////
+ {}              {}        / // /\  \ \____\    ->->->       ## \\       //     
+ {}              {}       /_//_/  \__\            ->        ##  /\\ ########### 
+ {}              {}                                            // \\    ##      
+  {}            {}                                            //   \\    ##     
+
+
+  '';
 
 }
