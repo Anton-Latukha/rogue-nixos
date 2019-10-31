c6d55e99016e98647b3750ac92f71053c6a7172f
Anton Latukha <anton.latukha@gmail.com>
2019-11-01 00:42:33 +0200

    upd conf

--- host/all.nix
+++ host/all.nix
@@ -3,7 +3,8 @@
   nixpkgs.config.allowUnfree = true;
 
   hardware.cpu.intel.updateMicrocode = true;
-  hardware.enableAllFirmware = true;
+  # hardware.enableAllFirmware = true;    # FIXME: 2019-11-01: Was not building due to: https://github.com/NixOS/nixpkgs/issues/71952 aka "error: cannot download osxupd10.11.5.dmg from any mirror"
+  enableRedistributableFirmware = true
 
   boot.cleanTmpDir = true;  # Clean /tmp on boot
   boot.tmpOnTmpfs = true;  # /tmp on ram drive
