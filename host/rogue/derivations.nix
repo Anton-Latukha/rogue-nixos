3da31a3a4abda637ed720ad18f5995d8a11a8fab
Anton Latukha <anton.latukha@gmail.com>
2020-03-08 19:46:52 +0200

    rogue: derivations: rm emacs override

--- host/rogue/derivations.nix
+++ host/rogue/derivations.nix
@@ -13,7 +13,6 @@ let
 
   # Import overrrides:
   # pkgs = import <nixpkgs> { config = import ./config.nix; };
-  pkgs.emacs.override { imagemagick = pkgs.imagemagickBig; };
 
   keepass-with-plugins = pkgs.keepass.override {
       plugins = [
