b1e78686963edc7d228a564d70b28a37eb0e80e9
Anton Latukha <anton.latukha@gmail.com>
2020-08-31 19:21:21 +0300

    upd conf

--- derivations.nix
+++ derivations.nix
@@ -449,6 +449,8 @@ in {
 
     debootstrap
 
+    master.haskell-language-server #  2020-08-31: NOTE: 0.2.0 did not worked (some Setup.hs issue), `master` had 0.3.0 version
+
   ] ++ (with haskellPackages; [
 
     #### Haskell packages
@@ -475,8 +477,6 @@ in {
     krank    # Tool lints comments in the code and tracks issue statuses
 
     hnix
-
-    master.haskell-language-server #  2020-08-31: NOTE: 0.2.0 did not worked (some Setup.hs issue), `master` had 0.3.0 version
   ])
 
   ;
